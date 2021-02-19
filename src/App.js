626a46f2557571850d5fce9dee47dd07c47cc214
HyunJong Song <songhj82@hotmail.com>
2021-02-19 19:23:38 +0900

    clean init project (functional / class)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+// rsc를 치면 프로젝트 기본모양 생성
+
+import React from 'react';
+
+const App = () => {
   return (
-    <div className="App">
-    <h2>hello world</h2>
-    </div>
+      <div>
+        <h1>hello world</h1>
+      </div>
   );
-}
+};
+
+export default App;
+
+// import React, {Component} from 'react';
+//
+// class App extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>hello</h1>
+//             </div>
+//         );
+//     }
+// }
 
 export default App;
