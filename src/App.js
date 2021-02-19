d65aa67477ce15d3dc9cb441877d1ce5ff8bc2c8
HyunJong Song <songhj82@hotmail.com>
2021-02-19 19:56:42 +0900

    4개 영화, 포스터 불러오고 -> 변환하기

--- src/App.js
+++ src/App.js
@@ -13,19 +13,60 @@ import React, {useState} from 'react';
 
 const App = () => {
     //상태값 선언
-    const [count, setCount] = useState("송현종")
+    const [count, setCount] = useState(
+        [
+            {
+                title: "Matrix",
+                poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+            },
+            {
+                title: "Full Metal Jacket",
+                poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+            },
+            {
+                title: "Oldboy",
+                poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+            },
+            {
+                title: "Star Wars",
+                poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+            }
+        ]
+    )
 
     //함수 선언
     const change = () => {
-        setCount("곽유신")
+        setCount([
+            {
+                title: "M",
+                poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY1200_CR84,0,630,1200_AL_.jpg"
+            },
+            {
+                title: "F",
+                poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+            },
+            {
+                title: "O",
+                poster: "https://upload.wikimedia.org/wikipedia/en/b/bb/Oldboy_2013_film_poster.jpg"
+            },
+            {
+                title: "S",
+                poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
+            }
+        ])
     }
     //변수 선언
 
     // 실제 화면에서 보여지는 부분
   return (
       <div>
-        <h1>{count}</h1>
-          <button type="button" onClick={change}> click me </button>
+        {count.map(movie => (
+            <>
+                <h1>{movie.title}</h1>
+                <img src={movie.poster} />
+            </>
+        ))}
+        <button type="button" onClick={change}> click me </button>
       </div>
   );
 
